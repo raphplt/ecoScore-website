1d17dc08e277e2976678ca4a241fe5eaa732f763
Raphael <plassa_r@etna-alternance.net>
2023-09-08 18:34:42 +0200

    ✨ Update comparatif

---
+++ src/components/searchBarTiny.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useContext, useEffect } from "react";
+import { api } from "../services/index";
+import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/dist/client/router";
+import { searchBar } from "@/services/products/products.services";
+import Link from "next/link";
+
+export default function SearchBarTiny() {
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+  const [suggest, setSuggest] = useState([]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const response = await searchBar(query);
+    if (router.pathname === "/comparatif") {
+      const data = localStorage.getItem("compare");
+      if (data) {
+        console.log("2 ");
+        localStorage.setItem("compare2", JSON.stringify(response));
+      } else {
+        console.log("1 ");
+        localStorage.setItem("compare", JSON.stringify(response));
+      }
+      router.reload();
+    } else {
+      localStorage.setItem("searchResults", JSON.stringify(response));
+      if (router.pathname === "/") {
+        router.push("/resultats");
+      } else router.reload();
+    }
+  };
+
+  const handleChange = async (e: any) => {
+    setQuery(e.target.value);
+    if (e.target.value === "") {
+      setSuggest([]);
+    } else if (e.target.value.length > 1) {
+      const result: any = await searchBar(e.target.value);
+      setSuggest(result);
+    }
+  };
+
+  const handleSetData = (data: any) => {
+    const item: any = localStorage.getItem("compare");
+    if (item) {
+      localStorage.setItem("compare2", JSON.stringify(data));
+    } else localStorage.setItem("compare", JSON.stringify(data));
+    router.reload();
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center border-2 rounded-3xl py-5 border-secondary-color hover:border-[#579452] w-[80%] sm:w-[70%] h-10 mx-auto"
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => handleChange(e)}
+          placeholder="Rechercher... "
+          className="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none pl-4"
+          required
+        />
+        {query.length > 0 && (
+          <button className="">
+            <XMarkIcon className="sm:h-6 sm:w-6 h-4 w-4" />
+          </button>
+        )}
+        <button
+          className="flex-shrink-0 bg-secondary-color hover:bg-[#579452] text-sm text-white py-2 px-2 rounded-3xl "
+          type="submit"
+        >
+          <MagnifyingGlassIcon className="sm:h-6 sm:w-6 h-4 w-4" />
+        </button>
+      </form>
+      <div className=" flex flex-col mt-6 mx-auto bg-opacity-40  ml-24 rounded-b-md">
+        {suggest &&
+          suggest.slice(0, 3).map((suggestion: any) => (
+            <div key={suggestion.id} className="py-1">
+              <button onClick={() => handleSetData(suggestion)}>
+                <strong>{suggestion.title.slice(0, query.length)}</strong>
+                {suggestion.title.slice(query.length)}
+              </button>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/comparatif.tsx
+++ src/pages/comparatif.tsx
@@ -4,14 +4,12 @@ import MetaData from "@/components/metadatas";
 import Score from "@/components/scores";
 import SearchBarComparatif from "@/components/searchBarComparatif";
 import SearchBarHeader from "@/components/searchBarHeader";
+import SearchBarTiny from "@/components/searchBarTiny";
 import { useEffect, useState } from "react";
 
 export default function Comparatif() {
-  const [idCompare1, setIdCompare1] = useState("");
   const [datas1, setDatas1]: any = useState([]);
   const [datas2, setDatas2]: any = useState([]);
-  const [score1, setScore1] = useState(0);
-  const [score2, setScore2] = useState(0);
 
   useEffect(() => {
     const item: any = localStorage.getItem("compare");
@@ -19,25 +17,18 @@ export default function Comparatif() {
   }, []);
 
   useEffect(() => {
-    const item = localStorage.getItem("compare2");
-    if (item) {
-      setDatas2(JSON.parse(item));
-      setScore2(
-        JSON.parse(item)[0].scoreEnergy +
-          JSON.parse(item)[0].scoreCarbon +
-          JSON.parse(item)[0].scoreRepair
-      );
-    }
+    const item: any = localStorage.getItem("compare2");
+    setDatas2(JSON.parse(item));
   }, []);
-
-  const clearDatas2 = () => {
-    localStorage.removeItem("compare2");
-    setDatas2([]);
-  };
 
   const clearDatas1 = () => {
     localStorage.removeItem("compare");
     setDatas1([]);
+  };
+
+  const clearDatas2 = () => {
+    localStorage.removeItem("compare2");
+    setDatas2([]);
   };
 
   return (
@@ -50,73 +41,88 @@ export default function Comparatif() {
           {!datas1 ? <SearchBarComparatif /> : <></>}
         </div>
         <div className="flex mx-auto w-11/12">
-          <div className="w-1/3 flex justify-aroundf mx-auto flex-col mb-96">
+          <div className="w-1/3 flex justify-aroundf mx-auto flex-col">
             {datas1 && JSON.stringify(datas1).length === 0 ? (
               <div>
                 <div className="text-center text-lg mb-6">
                   Choisir un produit à comparer
                 </div>
-                <SearchBarHeader />
+                <SearchBarTiny />
               </div>
             ) : (
-              <div className="text-center mb-8">
-                <h2 className="text-lg">
-                  {datas1 && JSON.stringify(datas1).length > 0 && datas1.title}
-                </h2>
+              <div className="text-center mb-5">
+                <h2 className="text-lg">{datas1 && datas1.title}</h2>
               </div>
             )}
             {/* {datas1 && JSON.stringify(datas1).length > 0 && ( */}
             <div className="flex flex-col">
               {/* <Score
-                  scoreEnergy={datas1.scoreEnergy}
-                  scoreCarbon={datas1.scoreCarbon}
-                  scoreRepair={datas1.scoreRepair}
-                /> */}
+                scoreEnergy={datas1.scoreEnergy}
+                scoreCarbon={datas1.scoreCarbon}
+                scoreRepair={datas1.scoreRepair}
+              /> */}
             </div>
             {/* )} */}
-            {datas1 && JSON.stringify(datas1).length !== 0 && (
+            {datas1 && (
               <button
                 onClick={clearDatas1}
-                className="mt-12 bg-slate-300 rounded-2xl py-1 drop-shadow-sm px-10 w-fit mx-auto"
+                className="bg-slate-300 rounded-2xl py-1 drop-shadow-sm px-5 w-fit mx-auto"
               >
                 Modifier le produit
               </button>
             )}
           </div>
-          <div className="w-1/3 flex justify-aroundf mx-auto flex-col mb-96">
-            {datas2 &&
-            datas2.length === 0 &&
-            datas1 &&
-            JSON.stringify(datas1).length > 0 ? (
+          <div className="w-1/3 flex justify-aroundf mx-auto flex-col ">
+            {!datas2 && datas1 ? (
               <div>
                 <div className="text-center text-lg mb-6">
                   Choisir un produit à comparer
                 </div>
-                <SearchBarHeader />
+                <SearchBarTiny />
               </div>
             ) : (
-              <h2 className="text-lg text-center mb-8">
-                {datas2 && datas2.length > 0 && datas2.title}
+              <h2 className="text-lg text-center mb-5">
+                {datas2 && datas2.title}
               </h2>
             )}
             <div className="flex flex-col">
-              {datas2.length > 0 && (
+              {/* {datas2 && JSON.stringify(datas2).length > 0 && (
                 <Score
                   scoreEnergy={datas2.scoreEnergy}
                   scoreCarbon={datas2.scoreCarbon}
                   scoreRepair={datas2.scoreRepair}
                 />
-              )}
+              )} */}
             </div>
-            {datas2.length > 0 && (
+            {datas2 && (
               <button
                 onClick={clearDatas2}
-                className="mt-12 bg-slate-300 rounded-2xl py-1 drop-shadow-sm px-10 w-fit mx-auto"
+                className="bg-slate-300 rounded-2xl py-1 drop-shadow-sm px-5 w-fit mx-auto"
               >
                 Modifier le produit
               </button>
             )}
           </div>
+        </div>
+        <div className="flex items-center gap-10 mt-12 flex-col">
+          {datas1 && datas1.scoreEnergy && datas2 && datas2.scoreEnergy && (
+            <div className="flex w-full mx-auto justify-around">
+              <div>{datas1.scoreEnergy}</div>
+              <div>{datas2.scoreEnergy}</div>
+            </div>
+          )}
+          {datas1 && datas1.scoreRepair && datas2 && datas2.scoreRepair && (
+            <div className="flex w-full mx-auto justify-around">
+              <div>{datas1.scoreRepair}</div>
+              <div>{datas2.scoreRepair}</div>
+            </div>
+          )}
+          {datas1 && datas1.scoreCarbon && datas2 && datas2.scoreCarbon && (
+            <div className="flex w-full mx-auto justify-around">
+              <div>{datas1.scoreCarbon}</div>
+              <div>{datas2.scoreCarbon}</div>
+            </div>
+          )}
         </div>
       </div>
 
